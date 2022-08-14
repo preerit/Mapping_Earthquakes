34faf2fa909685fd13eb20d91f1a1dfa94ef7fb2
PriyankaRit <priyankaritvij@outlook.com>
2022-08-14 10:53:52 +0530

    New changes

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,68 +1,26 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
+
 // We create the tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+// Then we add out 'streets' tile layer to the map
+streets.addTo(map);
 
-// Create a base layer that holds both maps.
-let baseMaps = {
-  Street: streets,
-  Dark: dark
-};
-
-// Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-  center: [44.0, -80.0],
-  zoom: 2,
-  layers: [streets]
-})
-
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/preerit/Mapping_Earthquakes/main/majorAirports.json";
 
-// Create a style for the lines.
-let myStyle = {
-  color: "#ffffa1",
-  weight: 2
-}
-
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(airportData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  style: myStyle,
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-    + feature.properties.dst + "</h3>")
-  }
-})
-.addTo(map);
+L.geoJSON(data).addTo(map);
 });
-
-
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-// Loop through the cities array and create one marker for each city.
-airportData.forEach(function(data) {
-    console.log(data)
-    L.marker(data.city)
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
-
